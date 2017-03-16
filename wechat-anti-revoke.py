7cb0d85db7e84a7fd879f1ebda9aabdfa4b113aa
Cong Ding <dinggnu@gmail.com>
2017-03-16 04:55:53 +0000

    remove

--- wechat-anti-revoke.py
+++ wechat-anti-revoke.py
@@ -74,8 +74,6 @@ def print_msg(msg):
     print json.dumps(msg).decode('unicode-escape').encode('utf8')
 
 def get_whole_msg(msg, download=False):
-    if msg['FileName'][-4:] == '.gif': # can't handle gif pictures
-        return []
     sender, receiver = get_sender_receiver(msg)
     if len(msg['FileName']) > 0 and len(msg['Url']) == 0:
         if download: # download the file into data_path directory
